b90836520c271ab110762cf8b2a87aa7a3695445
tdmosk <ted@Teds-iMac.local>
2013-11-17 21:39:00 -0500

    Initial Commit

---
+++ Naggr/Naggr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Naggr
+//
+//  Created by Ted on 11/17/13.
+//  Copyright (c) 2013 Ted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "tbdAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([tbdAppDelegate class]));
+    }
+}
---
+++ Naggr/Naggr/tbdAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  tbdAppDelegate.h
+//  Naggr
+//
+//  Created by Ted on 11/17/13.
+//  Copyright (c) 2013 Ted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tbdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
